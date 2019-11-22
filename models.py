494aa0f50c0157aa210106e6a4a35e5596b48a1a
Worker <yet_another@i.ru>
2019-11-23 00:28:05 +0300

    В очередной раз сломана база, а я не помню, что делал в прошлый раз. Сохраню. Работа по приведению проекта в соотвествие с документацие на 12.11.19.

--- models.py
+++ models.py
@@ -12,14 +12,9 @@ class User(UserMixin, database.Model):
     username = database.Column(database.String(64), index=True, unique=True)
     user_role = database.Column(database.String(64))
     password_hash = database.Column(database.String(128))
+    user_rating = database.Column(database.Integer)
 
-    collections = database.relationship('Collection', backref='author', lazy='dynamic')
-    document = database.relationship('Document', backref='author', lazy='dynamic')
-
-    # collections_create = database.relationship('Collection', backref='author', lazy='dynamic')
-    # collections_use = database.relationship('Collection', backref='user', lazy='dynamic')
-    # document_load = database.relationship('Document', backref='loader', lazy='dynamic')
-    # document_use = database.relationship('Document', backref='user', lazy='dynamic')
+    relation_with_document = database.relationship('Relation', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -31,33 +26,33 @@ class User(UserMixin, database.Model):
         self.password_hash = check_password_hash(self.password_hash, password)
 
 
-class Collection(database.Model):
+class Relation(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    id_author = database.Column(database.Integer, database.ForeignKey('user.id'))
-    id_handler = database.Column(database.Integer, database.ForeignKey('user.id'))
-    collection_name = database.Column(database.String(64), index=True)
-    date_create = database.Column(database.DateTime, index=True, default=datetime.utcnow)
-    deadline = database.Column(database.DateTime, index=True, default=datetime.utcnow)
-    task_description = database.Column(database.String(1000))
-    template_address = database.Column(database.String(256))
-    annotation_address = database.Column(database.String(256))
-
-    def __repr__(self):
-        return '<Collection {}>'.format(self.collection_name)
+    id_user = database.Column(database.Integer, database.ForeignKey('user.id'))
+    id_document = database.Column(database.Integer, database.ForeignKey('document.id'))
+    type_relation = database.Column(database.String(64))
 
 
 class Document(database.Model):
     id_document = database.Column(database.Integer, primary_key=True)
-    id_collection = database.Column(database.Integer, database.ForeignKey('collection.id'))
-    id_loader = database.Column(database.Integer, database.ForeignKey('user.id'))
-    id_handler = database.Column(database.Integer, database.ForeignKey('user.id'))
+    rubric = database.Column(database.String(140))
     description = database.Column(database.String(140))
     status = database.Column(database.String(64))
     path_to_file = database.Column(database.String(256))
     path_to_xml_file = database.Column(database.String(256))
 
+    relation_with_document = database.relationship('Relation', backref='author', lazy='dynamic')
+
     def __repr__(self):
         return '<Document {}>'.format(self.path_to_file)
+
+
+class Attributes(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    id_document
+    id_user
+    attributes_text = path_to_file = database.Column(database.String(1000))
+
 
 
 @login_manager.user_loader
