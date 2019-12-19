37663ad538d659be98e65a971473e36c3616c4fc
lubuntu_user <nope@nope.nope>
2019-12-19 15:54:56 +0300

    Исправлена регистрация, пользователи, докумениты.

--- forms.py
+++ forms.py
@@ -17,7 +17,7 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     user_role = StringField('USerRole', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Зарегестрироваться')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
