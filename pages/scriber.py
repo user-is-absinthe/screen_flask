576a91d2d4c2454330df5eae39c97b72c373b61c
owl <owl@owlmail.com>
2019-12-20 10:15:27 +0300

    Обрабатываем все документы.

--- pages/scriber.py
+++ pages/scriber.py
@@ -23,16 +23,24 @@ scribe_blueprint = Blueprint(
 )
 
 
+NEXT_ID_DICT = {
+    # user_id: doc_next_id
+}
+
+
 @scribe_blueprint.route('/scribe', methods=['GET', 'POST'])
-# @login_required
-@scribe_blueprint.route('/scribe/<next_text_id>', methods=['POST'])
+@login_required
+# @scribe_blueprint.route('/scribe/<next_text_id>', methods=['POST'])
 # @roles_required('admin')
 def scribe_page(next_text_id=None):
+    global NEXT_ID_DICT
     # TODO: проверяй передачу!!!
-    print(next_text_id)
+    print(123, next_text_id)
     form = ScribeForm()
 
     username, user_role = current_user.username, current_user.user_role
+    user_id = current_user.get_id()
+    print(user_id)
     user_relations = current_user.user_to_relation
     # print(user_relations)
 
@@ -55,23 +63,28 @@ def scribe_page(next_text_id=None):
         # print(123)
         # print(request.get_json(force=True))
         data_json = request.get_json(force=True)
+        print(data_json)
         current_text_id = data_json['NextIdCurrentText']
-        to_base = data_json['MapIdSelectAnnotateText']
-        # print(data_json['IdCurrentText'], current_text_id, type(to_base), to_base)
-        path_to_file =\
-            app.config['PATH_TO_ANN'] + str(current_user.id_user) + '_' + str(data_json['IdCurrentText']) + '.ann'
-        temp_doc = Document.query.get(data_json['IdCurrentText'])
-        temp_doc.id_document = data_json['IdCurrentText']
-        temp_doc.status = 1
-        temp_doc.path_to_xml_file = path_to_file
-        # database.session.add(temp_doc)
-        database.session.commit()
-        save_annotation(
-            path=path_to_file, data=to_base
-        )
+        NEXT_ID_DICT[user_id] = current_text_id
+        try:
+            to_base = data_json['MapIdSelectAnnotateText']
+            # print(data_json['IdCurrentText'], current_text_id, type(to_base), to_base)
+            path_to_file =\
+                app.config['PATH_TO_ANN'] + str(current_user.id_user) + '_' + str(data_json['IdCurrentText']) + '.ann'
+            temp_doc = Document.query.get(data_json['IdCurrentText'])
+            temp_doc.id_document = data_json['IdCurrentText']
+            temp_doc.status = 1
+            temp_doc.path_to_xml_file = path_to_file
+            # database.session.add(temp_doc)
+            database.session.commit()
+            save_annotation(
+                path=path_to_file, data=to_base
+            )
+        except KeyError:
+            pass
         # current_text, user_xml, user_collections = get_data_by_text_id(text_id=next_text_id)
         # current_text_id = next_text_id
-        return redirect(url_for('scribe_bp.scribe_page', next_text_id=current_text_id, _method='POST'))
+        return redirect(url_for('scribe_bp.scribe_page'))
     # user_docs_status.append(Document.query.get(r.document_id).get_status())
     # user_collections.append(opener(Document.query.get(r.document_id).get_rubric(), encoding='UTF-16'))
     # user_instructions.append(opener(Document.query.get(r.document_id).get_instruction()))
@@ -79,10 +92,14 @@ def scribe_page(next_text_id=None):
     # user_texts.append(opener(Document.query.get(r.document_id).get_text()))
     # user_xml.append(opener(Document.query.get(r.document_id).get_xml()))
     # TODO: удалить после тестов
-    if next_text_id:
-        current_text_id = next_text_id
-    else:
+    try:
+        current_text_id = NEXT_ID_DICT[user_id]
+    except KeyError:
         current_text_id = user_docs_ids[0]
+    # if next_text_id:
+    #     current_text_id = next_text_id
+    # else:
+    #     current_text_id = user_docs_ids[0]
 
     current_text, user_xml, user_collections = get_data_by_text_id(text_id=current_text_id)
     # print(user_collections)
@@ -119,6 +136,12 @@ def scribe_page(next_text_id=None):
         # имя пользователя и роль (yes)
         user_role=(username, user_role),
     )
+
+
+@scribe_blueprint.route('/test')
+def empty():
+    return redirect(url_for('scribe_bp.scribe_page'))
+    pass
 
 
 def get_data_by_text_id(text_id):
